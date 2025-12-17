b1f5edd781e24b16c67462faaa17b3d95514883c
Koushik-alt <p.deepthisudha@gmail.com>
2025-12-17 12:33:40 +0530

    added the sum function in data proceesing

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,7 @@
 print("data processing of fraud detection code")
+a=3
+b=2
+def sum(a,b):
+    c=a+b
+    return c
+print("wrong code bcz fn return a not c")
--- sample.py
+++ sample.py
@@ -1,7 +1,1 @@
 print("sample")
-a=3
-b=2
-def sum(a,b):
-   c=a+b
-   return a
-print("added a new functio sum which is not correct bcz it retuens a not sum")
